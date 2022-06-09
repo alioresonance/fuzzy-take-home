0cdf852ab1770182e0041946d35e4656e9194923
Gabe Zoldi <gabez@westmarine.com>
2022-06-09 15:49:36 -0700

    removed locator hard-coding

--- src/main/java/fuzzy/interview/pageobject/ProductPage.java
+++ src/main/java/fuzzy/interview/pageobject/ProductPage.java
@@ -15,6 +15,8 @@ public class ProductPage extends FuzzyPO {
     private static final By HEADER_TEXT = By.cssSelector(".title");
     private static final By SORT_DROPDOWN = By.cssSelector(".product_sort_container");
     private static final By PRODUCT_IMAGE = By.cssSelector("#item_4_img_link > img");
+    private static final By ADD_TO_CART_BUTTON = By.xpath("//button[text()='Add to cart']");
+    private static final By REMOVE_BUTTON = By.xpath("//button[text()='Remove']");
     private static final By PRICE_TEXT = By.cssSelector(".inventory_item_price");
     private static final By CART_COUNT_TEXT = By.cssSelector(".shopping_cart_badge");
 
@@ -29,7 +31,7 @@ public class ProductPage extends FuzzyPO {
         // TODO: product param not being used, or implemented, for sake of time
         // TODO: just click the first Add to cart button found on page
         findAll(By.cssSelector(".inventory_item")).stream().findAny().get()
-                .findElement(By.xpath("//button[text()='Add to cart']")).click();
+                .findElement(ADD_TO_CART_BUTTON).click();
         return this;
     }
 
@@ -37,7 +39,7 @@ public class ProductPage extends FuzzyPO {
         // TODO: product param not being used, or implemented, for sake of time
         // TODO: just click the first Remove button found on page
         findAll(By.cssSelector(".inventory_item")).stream().findAny().get()
-                .findElement(By.xpath("//button[text()='Remove']")).click();
+                .findElement(REMOVE_BUTTON).click();
         return this;
     }
 
